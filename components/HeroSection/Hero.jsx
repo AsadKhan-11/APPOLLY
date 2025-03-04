1001af913ccdd4fd4c80dcd08830e95d7e43e512
Asad Khan <mrasad10khan@gmail.com>
2025-03-05 01:31:07 +0500

    Create Hero Section

--- components/HeroSection/Hero.jsx
+++ components/HeroSection/Hero.jsx
@@ -1,14 +1,30 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <section className="relative w-[1525px] h-[1024px] mx-auto bg-[url('/Background-Image.svg')]">
-      <div className="w-[620px] h-[535px] absolute left-[135px] top-[321px] bg-transparent z-20 ">
-        <h2 className="w-auto h-auto" style={{ color: "royalblue" }}>
+    <section className="relative w-[1525px] h-[1024px]  ">
+      <Image
+        src="/Background-Image.svg"
+        alt="Background"
+        fill
+        className="absolute top-0 left-0 -z-10 w-[1440px] h-[1024px] object-cover"
+        priority
+      />
+      <div className="absolute w-[620px] h-[535px] left-[135px] top-[321px] bg-transparent z-20 ">
+        <h2
+          className="w-auto  font-bold leading-tight absolute left-[165px] top-[412px] text-left align-text-top"
+          style={{ color: "royalblue" }}
+        >
           A GREAT APP MAKES YOUR LIFE BETTER
         </h2>
-        <h6 className="h7">A GREAT APP MAKES YOUR LIFE BETTER</h6>
-        <h5>DOWNLOAD APP NOW</h5>
-        <div className="w-[314] h-[47] flex gap-3.5">
+        <h6 className="h7 w-[487px] h-[66px] absolute left-[165px] top-[566px] text-left align-text-top">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
+        </h6>
+        <h5 className="w-auto absolute left-[165px] top-[674px] font-medium text-left align-text-top">
+          DOWNLOAD APP NOW
+        </h5>
+        <div className="w-[314px] h-[47px] flex gap-3.5 absolute left-[165px] top-[717px]">
           <Image
             src="Google.svg"
             alt="Google-Image"
@@ -29,10 +45,10 @@ export default function Home() {
       </div>
       <div className="w-[570px] h-[432px] absolute left-[135px] top-[372px] bg-white z-10"></div>
       <div
-        className="w-[570px] h-[535px] absolute left-[185px] top-[321px] border-18 "
+        className="w-[570px] h-[535px] absolute left-[185px] top-[321px] border-[18px] "
         style={{
           borderColor: "#7572ff",
-          borderPosition: "inside",
+          inset: "inside",
         }}
       ></div>
 
