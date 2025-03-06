83b26ef9fddbe14f9e6ffa71f3ba65f2fc365989
Asad Khan <mrasad10khan@gmail.com>
2025-03-06 14:00:09 +0500

    Responsive Navbar for tablets

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Navbar() {
   return (
-    <nav className="w-[1171px] h-[114px] absolute left-[136px] top-[35px] ">
-      <div className="flex items-center justify-center gap-[18px] h-[24px] w-[183px] absolute top-0 left-0">
+    <nav
+      className=" w-[1171px] h-[114px] absolute left-[136px] top-[35px]
+    max-md:w-[688px] max-md:h-[134px] max-md:left-[40px]"
+    >
+      <div
+        className="flex items-center justify-center gap-[18px] h-[24px] w-[182px] absolute top-0 left-0
+          "
+      >
         {/* Email */}
         <Image
           src="/icons/Mail.svg"
@@ -19,7 +25,7 @@ export default function Navbar() {
       </div>
 
       {/* Phone details */}
-      <div className="flex items-center justify-center gap-[18px] h-[24px] w-[146px] absolute top-0 left-[230px]">
+      <div className="flex items-center justify-center gap-[18px] h-[24px] w-[145px] absolute top-0 left-[230px]">
         <Image
           src="/icons/Phone.svg"
           alt="Phone"
@@ -35,28 +41,45 @@ export default function Navbar() {
 
       {/* Navbar Links */}
 
-      <div className="absolute left-[0px] bottom-[0px] h-[90px] w-[1170px] flex items-center align-top text-left font-semibold text-[var(--grey-color)] ">
-        <h6 className="z-20 w-auto absolute left-[52px] bottom-[28px]">
+      <div
+        className="absolute left-[0px] bottom-[0px] h-[90px] w-[1170px] flex items-center align-top text-left font-semibold text-[var(--grey-color)]
+      max-md:w-[688px]  "
+      >
+        <h6
+          className="z-20 w-auto absolute left-[52px] bottom-[28px]
+         max-md:hidden"
+        >
           <Link href="/" className="w-auto">
             HOME
           </Link>
         </h6>
-        <h6 className="z-20 w-auto  absolute left-[155px] bottom-[28px] ">
+        <h6
+          className="z-20 w-auto  absolute left-[155px] bottom-[28px] 
+        max-md:hidden"
+        >
           <Link href="/" className="">
             ABOUT
           </Link>
         </h6>
-        <h6 className="z-20 w-auto absolute left-[266px] bottom-[28px]  ">
+        <h6
+          className="z-20 w-auto absolute left-[266px] bottom-[28px]  
+        max-md:hidden"
+        >
           <Link href="/" className="">
             FEATURES
           </Link>
         </h6>
 
         {/* Navbar logo */}
-        <div className="z-30 h-[90px] w-[200px] absolute left-[485px] flex justify-center items-center">
+        <div
+          className="z-30 h-[90px] w-[200px] absolute left-[485px] flex justify-center items-center
+       max-md:left-[240px] 
+          "
+        >
           <div
             className="z-20 h-[90px] w-[200px] bg-white absolute 
-        rounded-[5px]  shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"
+        rounded-[5px]  shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]
+                "
           ></div>
 
           <Image
@@ -64,17 +87,23 @@ export default function Navbar() {
             alt="Appolly Logo"
             height={28}
             width={167}
-            className="absolute left-[16px] top-[31px] z-30 h-[28px] w-[167px] "
+            className="absolute left-[16px] top-[31px] z-30 h-[28px] w-[167px]"
             priority
           />
         </div>
 
-        <h6 className="z-20 w-auto absolute left-[728px] bottom-[28px] ">
+        <h6
+          className="z-20 w-auto absolute left-[728px] bottom-[28px]
+        max-md:hidden "
+        >
           <Link href="/" className="">
             SCREENSHOT
           </Link>
         </h6>
-        <h6 className="z-20 w-auto absolute left-[906px] bottom-[28px] ">
+        <h6
+          className="z-20 w-auto absolute left-[906px] bottom-[28px] 
+        max-md:hidden"
+        >
           <Link href="/" className="">
             BLOG
           </Link>
@@ -96,11 +125,27 @@ export default function Navbar() {
         </div>
 
         {/* Nav background */}
-        <div className="w-[1170px] h-[60px] bg-white absolute z-0 rounded-[5px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+        <div
+          className="w-[1170px] h-[60px] bg-white absolute z-0 rounded-[5px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]
+        max-md:w-[688px] 
+        "
+        ></div>
+        {/* Mobile Menu Button */}
+        <Image
+          src="/icons/Menu.svg"
+          alt="Menu button"
+          height={24}
+          width={24}
+          className="z-30 absolute md:hidden block h-[24px] w-[24px] 
+          max-md:left-[18px] "
+        />
       </div>
 
       {/* Social Media Icons */}
-      <div className="flex gap-[30px] items-center w-[162px] h-[18px] absolute left-[1001px] top-[3px] ">
+      <div
+        className="flex gap-[30px] items-center w-[162px] h-[18px] absolute left-[1001px] top-[3px] 
+      max-md:left-[526px] "
+      >
         <Link
           href="/"
           className="w-[18px] h-[18px] flex items-center justify-center"
